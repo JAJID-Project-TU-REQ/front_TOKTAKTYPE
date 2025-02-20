89c8f08ab6994b057d27306ab9170fb17f1acaa6
jayminrp <rapeephanwadee.plo@dome.tu.ac.th>
2025-02-20 20:00:35 +0700

    font added logo added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,22 +19,25 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-900">
-      <div className="p-6 w-96 text-center bg-gray-800 text-white rounded-xl shadow-lg">
+    <div className="flex items-center justify-center min-h-screen bg-white">
+      <div className="p-6 w-96 text-center bg-white text-white rounded-xl">
       <Image
         src="/logo.png"
-        width={500}
-        height={500}
+        width={350}
+        height={300}
         alt="Picture of the author"
       />
         <h2 className="text-xl font-bold mb-4">Join or Create a Room</h2>
-        <input type="text" placeholder="enter your name" value={name} onChange={(e) => setName(e.target.value)} className="w-full p-2 mb-3 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-        <input type="text" placeholder="Enter room number" value={room} onChange={(e) => setRoom(e.target.value)}
-          className="w-full p-2 mb-3 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-        <button onClick={handleJoinRoom} className="w-full p-2 mb-2 bg-blue-500 hover:bg-blue-600 rounded">
-          Join Room
-        </button>
-        <button onClick={handleCreateRoom} className="w-full p-2 bg-green-500 hover:bg-green-600 rounded">
+        <input type="text" placeholder="Enter your name" value={name} onChange={(e) => setName(e.target.value)} className="w-full p-3 mb-3 rounded-xl text-white border border-stone-950 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+      
+        {/* Room number */}
+         <div className="mt-6 flex max-w-md gap-x-4 mb-4 bg-black p-4 rounded-3xl">
+          <label htmlFor="email-address" className="sr-only">Email address</label>
+          <input id="email-address" name="email" type="email" autoComplete="email" required className="min-w-0 flex-auto rounded-3xl bg-white px-3.5 py-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 sm:text-sm/6" placeholder="Enter your email" />
+          <button type="submit" className="flex-none rounded-mx px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-stone-800 rounded-sm focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Join room</button>
+        </div>
+
+        <button onClick={handleCreateRoom} className="w-10/12 p-2 bg-black hover:bg-stone-800 rounded-3xl">
           Create Room
         </button>
       </div>
